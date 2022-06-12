365638c338ed90254a86cc8f3a68f26762ce65bd
Zaid Ottmani <zaidott71@gmail.com>
2022-06-12 14:24:05 +0200

    html to php

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title></title>
+		<meta charset="UTF-8">
+    </head>
+
+
+<body>
+    
+   <?php include 'header.php'; ?>
+    <div> <br></div>
+
+    <!--section inscriptions-->
+
+    <h1> Actualités</h1>
+
+    <div></div>
+
+    <section class="blocks1">
+
+        <div class="block" align="center">
+            <h2>infos competitions</h2>
+            <a href="listecompetition.php">
+            <img src="images/start.jpg" href alt="" width="300">
+            </a>
+        </div>
+        <div class="block" align="center">
+            <h2>inscription aux triathlons</h2>
+            <a href="inscription1.php">
+            
+            <img src="images/inscri.jfif" alt="" width="300">
+            </a>
+        </div>
+        <div class="block" align="center">
+            <h2>authentification</h2>
+            <a href="connexion1.php">
+            
+            <img src="images/sign_in.png" alt="" width="300">
+            </a>
+        </div>
+
+    </section>
+
+    <!--section des 3 competitions-->
+
+    <h1> informez-vous : </h1>
+
+    <div></div>
+
+    <section class="blocks2">
+
+        <div class="block" align="center">
+            <a href="natation.php">
+            <h1>natation</h1>
+            <img src="images/swim.png" alt="" width="300">
+            </a>
+        </div>
+        <div class="block" align="center">
+        <a href="cyclisme.php">
+            <h1>cyclisme</h1>
+            <img src="images/cycle1.jpg" alt="" width="300">
+            </a>
+        </div>
+        <div class="block" align="center">
+        <a href="course.php">
+            <h1>course</h1>
+            <img src="images/run.png" alt="" width="300">
+            </a>
+        </div>
+
+    </section>
+    <div class="space">
+        <br><br>
+    </div>
+
+    <footer>
+        <p><i class="fa fa-copyright" aria-hidden="true"> contactez-nous au 05 90 32 45 61</i></p>
+        <div class="social-media">
+            <p> <i class="fab fa-facebook-f" aria-hidden="true"></i></p>
+            <p> <i class="fab fa-twitter" aria-hidden="true"></i></p>
+            <p> <i class="fab fa-instagram" aria-hidden="true"></i></p>
+            <p> <i class="fab fa-linkedin" aria-hidden="true"></i></p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
